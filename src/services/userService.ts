36ff88714089a7216a40637fee0e468ed8970cb8
raphael  Tirola <35945895-raphaeldacruz13@users.noreply.replit.com>
2024-10-09 23:44:05 +0000

    criando o helper

---
+++ src/services/userService.ts
@@ -0,0 +1,21 @@
+import { isValidEmail } from "../helpers/validationHelper";
+import { UserRepository } from "../repositories/userRepository";
+
+export class UserService {
+  private userRepository: UserRepository;
+
+  constructor() {
+    this.userRepository = new UserRepository();
+  }
+
+  async createUser(name: string, email: string) {
+    if (!isValidEmail(email)) {
+      throw new Error("Email inválido");
+    }
+    return await this.userRepository.addUser(name, email);
+  }
+
+  async listUsers() {
+    return await this.userRepository.getAllUsers();
+  }
+}
